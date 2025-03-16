1b2cdd0a553f8ba71053006de1a994c4f3a2b75b
akash <akash.anan03@gmail.com>
2025-03-16 15:44:09 +0530

    fix: gdt 0x30 general protection issue

--- twilight_kernel/src/arch/x86_64/gdt.rs
+++ twilight_kernel/src/arch/x86_64/gdt.rs
@@ -2,6 +2,7 @@ use lazy_static::lazy_static;
 use x86_64::structures::gdt::{Descriptor, GlobalDescriptorTable, SegmentSelector};
 use x86_64::structures::tss::TaskStateSegment;
 use x86_64::VirtAddr;
+use crate::{println, print};
 
 pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
 
@@ -10,13 +11,11 @@ lazy_static! {
         let mut tss = TaskStateSegment::new();
         tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
             const STACK_SIZE: usize = 4096 * 5;
-            #[repr(align(16))]
-            struct AlignedStack([u8; STACK_SIZE]);
-
-            static mut STACK: AlignedStack = AlignedStack([0; STACK_SIZE]);
+            static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
 
             let stack_start = VirtAddr::from_ptr(&raw const STACK);
             let stack_end = stack_start + STACK_SIZE as u64;
+            println!("{}", stack_end.as_u64());
             stack_end
         };
         tss
@@ -26,14 +25,19 @@ lazy_static! {
 lazy_static! {
     static ref GDT: (GlobalDescriptorTable, Selectors) = {
         let mut gdt = GlobalDescriptorTable::new();
+
         let code_selector = gdt.append(Descriptor::kernel_code_segment());
+
+        gdt.append(Descriptor::kernel_data_segment());
+
         let tss_selector = gdt.append(Descriptor::tss_segment(&TSS));
-        (
-            gdt,
-            Selectors {
-                code_selector,
-                tss_selector,
-            },
+
+        gdt.append(Descriptor::UserSegment(0));
+
+        gdt.append(Descriptor::kernel_data_segment());
+
+        (gdt,
+            Selectors { code_selector, tss_selector },
         )
     };
 }
