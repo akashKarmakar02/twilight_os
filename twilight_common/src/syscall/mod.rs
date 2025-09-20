f1f2d1920b9836c7a33aad637615f40cbdf3e673
akashKarmakar02 <akash.anan03@gmail.com>
2025-09-20 19:11:55 +0530

    update: syscall wrapper

--- twilight_common/src/syscall/mod.rs
+++ twilight_common/src/syscall/mod.rs
@@ -2,82 +2,97 @@ pub mod numbers;
 pub mod types;
 
 use core::arch::asm;
-
 #[doc(hidden)]
 pub unsafe fn syscall0(n: usize) -> usize {
-    unsafe {
-        let res: usize;
-        asm!(
-        "int 0x80", in("rax") n,
-        lateout("rax") res
-        );
-        res
-    }
+    let mut res: usize;
+    asm!(
+    "syscall",
+    inlateout("rax") n => res,
+    lateout("rcx") _,    // clobbered by `syscall`
+    lateout("r11") _,    // clobbered by `syscall`
+    options(nostack)
+    );
+    res
 }
 
 #[doc(hidden)]
 pub unsafe fn syscall1(n: usize, arg1: usize) -> usize {
-    unsafe {
-        let res: usize;
-        asm!(
-        "int 0x80", in("rax") n,
-        in("rdi") arg1,
-        lateout("rax") res
-        );
-        res
-    }
+    let mut res: usize;
+    core::arch::asm!(
+    "syscall",
+    inlateout("rax") n => res,
+    in("rdi") arg1,
+    lateout("rcx") _,
+    lateout("r11") _,
+    options(nostack)
+    );
+    res
 }
 
 #[doc(hidden)]
 pub unsafe fn syscall2(n: usize, arg1: usize, arg2: usize) -> usize {
-    unsafe {
-        let res: usize;
-        asm!(
-        "int 0x80", in("rax") n,
-        in("rdi") arg1, in("rsi") arg2,
-        lateout("rax") res
-        );
-        res
-    }
+    let mut res: usize;
+    asm!(
+    "syscall",
+    inlateout("rax") n => res,
+    in("rdi") arg1,
+    in("rsi") arg2,
+    lateout("rcx") _,
+    lateout("r11") _,
+    options(nostack)
+    );
+    res
 }
 
 #[doc(hidden)]
 pub unsafe fn syscall3(n: usize, arg1: usize, arg2: usize, arg3: usize) -> usize {
-    unsafe {
-        let res: usize;
-        asm!(
-        "int 0x80", in("rax") n,
-        in("rdi") arg1, in("rsi") arg2, in("rdx") arg3,
-        lateout("rax") res
-        );
-        res
-    }
+    let mut res: usize;
+    asm!(
+    "syscall",
+    inlateout("rax") n => res,
+    in("rdi") arg1,
+    in("rsi") arg2,
+    in("rdx") arg3,
+    lateout("rcx") _,
+    lateout("r11") _,
+    options(nostack)
+    );
+    res
 }
 
 #[doc(hidden)]
 pub unsafe fn syscall4(n: usize, arg1: usize, arg2: usize, arg3: usize, arg4: usize) -> usize {
-    unsafe {
-        let res: usize;
-        asm!(
-        "int 0x80", in("rax") n,
-        in("rdi") arg1, in("rsi") arg2, in("rdx") arg3, in("r10") arg4,
-        lateout("rax") res
-        );
-        res
-    }
+    let mut res: usize;
+    asm!(
+    "syscall",
+    inlateout("rax") n => res,
+    in("rdi") arg1,
+    in("rsi") arg2,
+    in("rdx") arg3,
+    in("r10") arg4,   // 4th arg goes in r10 (NOT rcx)
+    lateout("rcx") _,
+    lateout("r11") _,
+    options(nostack)
+    );
+    res
 }
 
 #[doc(hidden)]
 pub unsafe fn syscall5(n: usize, arg1: usize, arg2: usize, arg3: usize, arg4: usize, arg5: usize) -> usize {
-    unsafe {
-        let res: usize;
-        asm!(
-        "int 0x80", in("rax") n,
-        in("rdi") arg1, in("rsi") arg2, in("rdx") arg3, in("r10") arg4, in("r8") arg5,
-        lateout("rax") res
-        );
-        res
-    }
+    let mut res: usize;
+    asm!(
+    "syscall",
+    inlateout("rax") n => res,
+    in("rdi") arg1,
+    in("rsi") arg2,
+    in("rdx") arg3,
+    in("r10") arg4,
+    in("r8")  arg5,
+    lateout("rcx") _,
+    lateout("r11") _,
+    options(nostack)
+    );
+    res
 }
 
 #[macro_export]
