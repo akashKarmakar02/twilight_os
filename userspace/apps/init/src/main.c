88b743b0cad4698121f3459b4d801d21f983ec73
Ananya-Sa <ananya.sa2002@gmail.com>
2026-02-02 18:51:43 +0530

    remove debug log

--- userspace/apps/init/src/main.c
+++ userspace/apps/init/src/main.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("Init process started (PID 1)\n");
+   
 
     int pid = fork();
 
@@ -14,14 +14,13 @@ int main() {
 
     if (pid == 0) {
         char *argv[] = { "/bin/tsh", NULL };
-        char *envp[] = { "PATH=/bin", "HOME=/", NULL }; 
-        printf("Executing /bin/tsh\n");
+        char *envp[] = { "PATH=/bin", "HOME=/", NULL };
         execve("/bin/tsh", argv, envp);
         printf("Exec failed!\n");
         return 1;
     }
 
-    printf("Init parent waiting for child %d\n", pid);
+   
     while (1) {
         int status;
         int res = waitpid(-1, &status, 0); // Wait for ANY child
