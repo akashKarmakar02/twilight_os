7a2837d795da130cc3280b38e5a9ccd4aa4eee5e
akashKarmakar02 <akash.anan03@gmail.com>
2026-02-02 23:55:00 +0530

    update: init system

--- userspace/apps/init/src/main.c
+++ userspace/apps/init/src/main.c
@@ -1,44 +1,71 @@
-#include <unistd.h>
-#include <sys/wait.h>
+#include <dirent.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int has_users() {
+  DIR *d = opendir("/home");
+  if (!d)
+    return 0;
+
+  struct dirent *dir;
+  int found = 0;
+  while ((dir = readdir(d)) != NULL) {
+    if (strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0 &&
+        dir->d_type == DT_DIR) {
+      found = 1;
+      break;
+    }
+  }
+  closedir(d);
+  return found;
+}
+
+void launch_shell_or_login() {
+  int user_exists = has_users();
+
+  if (user_exists) {
+    char *argv[] = {"/bin/logind", NULL};
+    char *envp[] = {"PATH=/bin", "HOME=/", NULL};
+    execve("/bin/logind", argv, envp);
+  } else {
+    char *argv[] = {"/bin/tsh", NULL};
+    char *envp[] = {"PATH=/bin", "HOME=/", NULL};
+    execve("/bin/tsh", argv, envp);
+  }
+}
 
 int main() {
-   
+  int pid = fork();
 
-    int pid = fork();
+  if (pid < 0) {
+    printf("Init fork failed\n");
+    return 1;
+  }
 
-    if (pid < 0) {
-        printf("Init fork failed\n");
-        return 1;
-    }
+  if (pid == 0) {
+    launch_shell_or_login();
+    printf("Exec failed!\n");
+    return 1;
+  }
 
-    if (pid == 0) {
-        char *argv[] = { "/bin/tsh", NULL };
-        char *envp[] = { "PATH=/bin", "HOME=/", NULL };
-        execve("/bin/tsh", argv, envp);
-        printf("Exec failed!\n");
-        return 1;
-    }
+  while (1) {
+    int status;
+    int res = waitpid(-1, &status, 0); // Wait for ANY child
+    if (res > 0) {
+      printf("Child %d exited with status %d, reaping...\n", res, status);
 
-   
-    while (1) {
-        int status;
-        int res = waitpid(-1, &status, 0); // Wait for ANY child
-        if (res > 0) {
-             printf("Child %d exited with status %d, reaping...\n", res, status);
-             
-             if (res == pid) { // The shell we spawned
-                 printf("Shell exited, restarting...\n");
-                 pid = fork();
-                 if (pid == 0) {
-                     char *argv[] = { "/bin/tsh", NULL };
-                     char *envp[] = { "PATH=/bin", "HOME=/", NULL };
-                     execve("/bin/tsh", argv, envp);
-                     return 1; 
-                 }
-                 printf("Restarted shell with PID %d\n", pid);
-             }
+      if (res == pid) { // The process we spawned
+        printf("Process exited, restarting...\n");
+        pid = fork();
+        if (pid == 0) {
+          launch_shell_or_login();
+          return 1;
         }
+        printf("Restarted process with PID %d\n", pid);
+      }
     }
-    return 0;
+  }
+  return 0;
 }
--- userspace/apps/install/src/main.c
+++ userspace/apps/install/src/main.c
@@ -1,5 +1,23 @@
+#include <stdio.h>
 #include <unistd.h>
 
 int main() {
-    syscall(700);
+  syscall(700);
+
+  char username[64];
+  printf("\nInstallation complete.\n");
+  printf("Enter username to login: ");
+  fflush(stdout);
+
+  if (scanf("%63s", username) == 1) {
+    char *argv[] = {"/bin/logind", "-u", username, NULL};
+    execv("/bin/logind", argv);
+    perror("execv failed");
+  } else {
+    printf("Failed to read username\n");
+  }
+  printf("It is recommended to reboot the system now.\n");
+  fflush(stdout);
+
+  return 0;
 }
